2718aae21baede2dde1167f1fb52baf1e4b9fcc3
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-09-26 10:02:23 +0200

    login register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,78 @@
+import { LogoutOutlined } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <h1>LoginPage</h1>;
+  const [email, setEmail] = useState("info@pehlione.com");
+  const [password, setPassword] = useState("12345");
+
+  //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+  const handleSubmitEmail = (e) => {
+    if (e.key === "Enter") {
+      console.log("Enter'a basıldı email: ", email);
+    }
+  };
+  const handleSubmitPassword = (e) => {
+    if (e.key === "Enter") {
+      console.log("Enter'a basıldı password: ", password);
+    }
+  };
+  return (
+    <Container>
+      <h1>Login Page</h1>
+      <Paper sx={{ p: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, bgcolor: "secondary.main" }}>
+          <LogoutOutlined />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          align="center"
+          mb={2}
+          sx={{ color: "secondary.dark" }}
+        >
+          Giriş Yap
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ mb: 2, display: "flex", flexDirection: "column", gap: 2 }}
+        >
+          <TextField
+            value={email}
+            onSubmit={handleSubmitEmail}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email"
+            type="email"
+            fullWidth
+            required
+          />
+          <TextField
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onSubmit={handleSubmitPassword}
+            label="Password"
+            type="password"
+            fullWidth
+            required
+          />
+          <Button type="submit" variant="contained" color="secondary" fullWidth>
+            Giriş Yap
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,47 @@
+import { LogoutOutlined } from "@mui/icons-material";
+import { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
+
 export default function RegisterPage() {
-  return <h1>RegisterPage</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <Container>
+      <h1>Register Page</h1>
+      <Paper sx={{ p: 2 }} elevation={3}>
+        <Avatar sx={{ mx: "auto", mb: 2, bgcolor: "secondary.main" }}>
+          <LogoutOutlined />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          align="center"
+          mb={2}
+          sx={{ color: "secondary.dark" }}
+        >
+          Giriş Yap
+        </Typography>
+        <Box
+          component="form"
+          sx={{ mb: 2, display: "flex", flexDirection: "column", gap: 2 }}
+        >
+          <TextField label="Username" type="text" fullWidth required />
+          <TextField label="Email" type="email" fullWidth required />
+          <TextField label="Password" type="password" fullWidth required />
+          <Button type="submit" variant="contained" color="secondary" fullWidth>
+            Kayıt Ol
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
