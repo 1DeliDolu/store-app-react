94f5173855f7051095619e6e2523c682c201bd5c
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-09-27 11:37:43 +0200

    Stepper

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,10 +1,43 @@
-import { Grid2, Paper } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid2,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from "@mui/material";
 import Info from "./Info";
 import AddressForm from "./AddressForm";
 import PaymentForm from "./PaymentForm";
 import Review from "./Review";
+import { useState } from "react";
+import { ChevronLeftRounded, ChevronRightRounded } from "@mui/icons-material";
+
+const steps = ["Teslimat Bilgileri", "Ödeme", "Sipariş Özeti"];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <PaymentForm />;
+    case 2:
+      return <Review />;
+  }
+}
 
 export default function CheckoutPage() {
+  const [activeStep, setActiveStep] = useState(0);
+
+  function handlePrevious() {
+    setActiveStep(activeStep - 1);
+  }
+
+  function handleNext() {
+    setActiveStep(activeStep + 1);
+  }
   return (
     <Paper>
       <Grid2 container spacing={3}>
@@ -15,9 +48,49 @@ export default function CheckoutPage() {
           <Info />
         </Grid2>
         <Grid2 size={8} sx={{ p: 3 }}>
-          <AddressForm />
-          <PaymentForm />
-          <Review />
+          <Stepper activeStep={activeStep} sx={{ height: 40, mb: 4 }}>
+            {steps.map((label) => (
+              <Step key={label} sx={{ color: "secondary" }}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <Typography variant="h5">Siparişinizi aldık.</Typography>
+          ) : (
+            <>
+              {getStepContent(activeStep)}
+
+              <Box
+                sx={[
+                  { display: "flex" },
+                  activeStep !== 0
+                    ? { justifyContent: "space-between" }
+                    : { justifyContent: "flex-end" },
+                ]}
+              >
+                {activeStep !== 0 && (
+                  <Button
+                    onClick={handlePrevious}
+                    startIcon={<ChevronLeftRounded />}
+                    variant="contained"
+                    color="secondary"
+                  >
+                    Geri
+                  </Button>
+                )}
+
+                <Button
+                  onClick={handleNext}
+                  startIcon={<ChevronRightRounded />}
+                  variant="contained"
+                  color="secondary"
+                >
+                  İleri
+                </Button>
+              </Box>
+            </>
+          )}
         </Grid2>
       </Grid2>
     </Paper>
